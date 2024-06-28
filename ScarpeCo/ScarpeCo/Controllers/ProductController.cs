48818ca8b10cdd178261e596b2c08aac043849a4
Luca Chisari <75541078+Eddy7642@users.noreply.github.com>
2024-06-28 15:20:21 +0200

    Commit 1

---
+++ ScarpeCo/ScarpeCo/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScarpeCo.Models;
+using ScarpeCo.Repositories;
+
+namespace ScarpeCo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = ProductRepository.GetAll();
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = ProductRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+}
+
---
+++ ScarpeCo/ScarpeCo/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ScarpeCo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScarpeCo.Repositories
+{
+    public static class ProductRepository
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Scarpa A", Price = 49.99m, Description = "Scarpa A dettagliata", CoverImage = "cover1.jpg", AdditionalImage1 = "add1.jpg", AdditionalImage2 = "add2.jpg" },
+            new Product { Id = 2, Name = "Scarpa B", Price = 59.99m, Description = "Scarpa B dettagliata", CoverImage = "cover2.jpg", AdditionalImage1 = "add3.jpg", AdditionalImage2 = "add4.jpg" },
+        };
+
+        public static List<Product> GetAll() => products;
+
+        public static Product? GetById(int id) => products.FirstOrDefault(p => p.Id == id);
+    }
+}
